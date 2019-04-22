1bfe14e8bd56cb684f08a0ad6fd8b2a9aecbba4b
Phillip Schmidt <pjs.schmidt@gmail.com>
2019-04-22 16:37:18 -0500

    initial commit

---
+++ uMove.cpp
@@ -0,0 +1,20 @@
+/*
+      uMove -- Simple one dimentional motion planner
+
+      Copyright (C) 2016  Phillip J Schmidt
+ */
+
+
+#include "uMove.h"
+
+
+uMove::uMove()
+{
+
+}
+
+
+uMove::~uMove()
+{
+
+}
---
+++ uMove.h
@@ -0,0 +1,29 @@
+/*
+      uMove -- Simple one dimentional motion planner
+      
+      Copyright (C) 2016  Phillip J Schmidt
+ */
+
+
+#ifndef uMove_h
+   #define uMove_h
+
+   #include "Arduino.h"
+
+
+   class uMove
+   {
+      public:
+         uMove();
+         ~uMove();
+
+
+      private:
+
+
+   };
+
+
+
+
+#endif
